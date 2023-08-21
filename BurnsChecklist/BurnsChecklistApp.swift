6da2c1fd5cd63e52831cca7a47cacdd5f4bd9f7c
Robert Doggett <robert@peacedudes.com>
2023-08-21 11:22:36 -0700

    Initial Commit

---
+++ BurnsChecklist/BurnsChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurnsChecklistApp.swift
+//  BurnsChecklist
+//
+//  Created by robert on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct BurnsChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BurnsChecklistUITests/BurnsChecklistUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BurnsChecklistUITestsLaunchTests.swift
+//  BurnsChecklistUITests
+//
+//  Created by robert on 8/21/23.
+//
+
+import XCTest
+
+final class BurnsChecklistUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
